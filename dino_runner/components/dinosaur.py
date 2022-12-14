5f807bbc869db9d138913ebbfaf1da9da1a0c6de
Marllon-27 <marllon27_@hotmail.com>
2022-12-14 16:44:04 -0300

    Marlon - duck
    Foi add ação de baixar.

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,11 +1,12 @@
 import pygame
 
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 RUN_IMG = [RUNNING[0], RUNNING[1]]
 X_POS = 80
 Y_POS = 310
 JUMP_VEL = 8.5
+Y_DUCK_POS = 340
 
 class Dinosaur:
     def __init__(self):
@@ -17,6 +18,7 @@ class Dinosaur:
         self.running: bool = False
         self.jump: bool = False
         self.jump_vel = JUMP_VEL
+        self.ducking = False
 
     def update(self, user_input):
 
@@ -24,13 +26,19 @@ class Dinosaur:
              self.run()
         elif self.jump:
             self.dino_jump()
+        elif self.ducking:
+            self.duck()
 
         if user_input[pygame.K_UP] and not self.jump:
             self.running = False
             self.jump = True
-        elif not self.jump:
+        elif user_input[pygame.K_DOWN] and not self.jump:
             self.jump = False
-            self.running = True
+            self.running = False 
+            self.ducking = True     
+        elif not self.jump and not self.ducking:
+            self.jump = False
+            self.running = True        
 
         if self.step_index >= 9:
            self.step_index = 0
@@ -62,7 +70,20 @@ class Dinosaur:
             self.jump_vel = JUMP_VEL
           
     def duck(self):
-        pass
+        
+        if self.step_index < 5:
+            self.image = DUCKING[0]
+            self.dino_rect = self.image.get_rect()
+            self.dino_rect.x = X_POS
+            self.dino_rect.y = Y_DUCK_POS
+        else:
+            self.image = DUCKING[1]
+            self.dino_rect = self.image.get_rect()
+            self.dino_rect.x = X_POS
+            self.dino_rect.y = Y_DUCK_POS            
+        
+        self.step_index += 1
+        self.ducking = False        
 
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
